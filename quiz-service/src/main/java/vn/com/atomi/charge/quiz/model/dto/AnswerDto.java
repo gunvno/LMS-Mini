be270679b0932b490e27f898f2b66f54278e5a88
gunvno <gunvno2214@gmail.com>
2026-07-09 10:53:27 +0700

    update

--- quiz-service/src/main/java/vn/com/atomi/charge/quiz/model/dto/AnswerDto.java
+++ quiz-service/src/main/java/vn/com/atomi/charge/quiz/model/dto/AnswerDto.java
@@ -1,14 +1,11 @@
 package vn.com.atomi.charge.quiz.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import vn.com.atomi.charge.base.model.dto.BaseDto;
-import vn.com.atomi.charge.quiz.model.enums.QuizStatus;
 
 @Getter
 @Setter
@@ -23,11 +20,11 @@ public class AnswerDto extends BaseDto<String> {
     @Schema(description = "content of answer", example = "Learn ApiRestful")
     private String content;
 
-    @NotBlank(groups = Create.class)
+    @NotNull(groups = Create.class)
     @Schema(description = "correct of answer", example = "true")
     private Boolean correct;
 
-    @NotBlank(groups = Create.class)
+    @NotNull(groups = Create.class)
     @Schema(description = "where answer is putted in", example = "1")
     private Integer orderIndex;
 }
--- quiz-service/src/main/java/vn/com/atomi/charge/quiz/model/dto/QuizDto.java
+++ quiz-service/src/main/java/vn/com/atomi/charge/quiz/model/dto/QuizDto.java
@@ -1,10 +1,10 @@
 package vn.com.atomi.charge.quiz.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import vn.com.atomi.charge.base.model.dto.BaseDto;
@@ -28,18 +28,19 @@ public class QuizDto extends BaseDto<String> {
     @Schema(description = "Title of quiz", example = "Learn ApiRestful")
     private String title;
 
-    @NotBlank(groups = Create.class)
+    @NotNull(groups = Create.class)
     @Schema(description = "PassScore of quiz", example = "70")
     private BigDecimal passScore;
 
-    @NotBlank(groups = Create.class)
+    @NotNull(groups = Create.class)
     @Schema(description = "MaxAttempts of quiz", example = "6")
     private Integer maxAttempts;
 
-    @NotBlank(groups = Create.class)
+    @NotNull(groups = Create.class)
     @Schema(description = "requiredToComplete of quiz", example = "true")
     private Boolean requiredToComplete;
 
+    @NotNull(groups = Create.class)
     @Schema(description = "status of quiz", example = "ACTIVE")
     @Enumerated(EnumType.STRING)
     private QuizStatus status;
